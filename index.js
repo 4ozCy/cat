7dc5f26cc13d82d96ae324e992c37fc228eeaf63
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-03-09 23:03:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
 const { WebhookClient } = require('discord.js');
 
-const webhookURL = 'https://discord.com/api/webhooks/13483109853159792'
+const webhookURL = 'https://discord.com/api/webhooks/1348310985315979285/MPpJ4grERXWz300wio1-MUlOs44xa1AS1yOi6oQIj8CtnoO-mhVRLiBmjfQkCAYN2_2s'
 const webhookClient = new WebhookClient({ url: webhookURL });
 
 async function sendCatImage() {
     try {
-        const response = await axios.get('https://api.thecatapi.com/v1>
+        const response = await axios.get('https://api.thecatapi.com/v1/images/search'
         if (response.data.length > 0) {
-            const catImage = response.data[0].url; // Extract the imag>
+            const catImage = response.data[0].url;
             await webhookClient.send({
-                embeds: [{ image: { url: catImage } }] // Embed with o>
+                embeds: [{ image: { url: catImage } }]
             });
             console.log('Cat image sent!');
         } else {
@@ -21,5 +21,4 @@ async function sendCatImage() {
     }
 }
 
-// Send a cat image every 5 seconds
 setInterval(sendCatImage, 5000);
