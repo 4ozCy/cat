0cf92080716b847e19a1c38d53f72d23792c2927
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-03-09 23:22:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const axios = require('axios');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 const { WebhookClient } = require('discord.js');
+
+app.get('/', (req, res) => {
+    res.send('cat');
+});
 
 const webhookURL = 'https://discord.com/api/webhooks/1348310985315979285/MPpJ4grERXWz300wio1-MUlOs44xa1AS1yOi6oQIj8CtnoO-mhVRLiBmjfQkCAYN2_2s'
 const webhookClient = new WebhookClient({ url: webhookURL });
@@ -22,3 +29,7 @@ async function sendCatImage() {
 }
 
 setInterval(sendCatImage, 5000);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
